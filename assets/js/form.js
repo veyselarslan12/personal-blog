57c65840e81ada0fe5ff5ad30b9749feb2cff6be
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-03-25 02:11:12 -0500

    added javascript codes

---
+++ assets/js/form.js
@@ -0,0 +1,14 @@
+const username = document.querySelector('.form-username')
+const title = document.querySelector('.form-title')
+const content = document.querySelector('.form-content')
+const submit = document.getElementById('submit')
+const imageLink = document.querySelector("a")
+
+
+// imageLink.setAttribute('href', 'https://google.com')
+
+
+console.log(username)
+console.log(title)
+console.log(content)
+console.log(submit)
