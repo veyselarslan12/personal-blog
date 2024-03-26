37a4bc33c315b25d0a675fd8b38bbf5fe531dbd9
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-03-26 01:23:39 -0500

    form and blog javascript codes are typed

---
+++ assets/js/blog.js
@@ -0,0 +1,37 @@
+
+
+
+function savedFormData() {
+    const blogTitle = document.querySelector('blog-title-text')
+    const blogContent = document.querySelector('blog-content-text')
+    const blogUsername = document.querySelector('blog-username-text')
+
+    const savedData = JSON.parse(localStorage.getItem(formData))
+
+    if  (savedData !== null) {
+        blogTitle.innerText = savedData.title,
+        blogUsername.innerText = savedData.username,
+        blogContent.innerText = savedData.content
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const targetDiv = document.getElementById('target-element')
+
+// create element
+    // const p = document.createElement('p')
+
+// append into DOM
+    // targetDiv.appendChild(p)
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,14 +1,29 @@
-const username = document.querySelector('.form-username')
-const title = document.querySelector('.form-title')
-const content = document.querySelector('.form-content')
-const submit = document.getElementById('submit')
-const imageLink = document.querySelector("a")
+const form = document.getElementById('myForm')
+const username = document.getElementById('username')
+const title = document.getElementById('title')
+const content = document.getElementById('content')
+const submitButton = document.getElementById('form-submit')
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault() // prevent the default submit
+
+    const formData = {
+        username: username.value,
+        title: title.value,
+        content: content.value
+    }
+
+     // save data to Local Storage
+    localStorage.setItem('formData',JSON.stringify(formData)) 
+    // Clear the form
+    form.reset() 
+
+})
 
 
-// imageLink.setAttribute('href', 'https://google.com')
 
 
-console.log(username)
-console.log(title)
-console.log(content)
-console.log(submit)
+
+
+
+
